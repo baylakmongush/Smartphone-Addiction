5322bf783cae2106e8a128aa3f706102095cdf2c
jterrell <belenko.ivan@inbox.ru>
2020-05-02 18:14:00 +0300

    prefabs and inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+	private Canvas canvas;
+
+	public GameObject player;
+
+	private Items items;
+
+	public Transform inventorySlots;
+
+	private Slot[] slots;
+	public Sprite item;
+	void Start()
+	{
+		canvas = GetComponent<Canvas>();
+		canvas.enabled = false;
+		items = player.GetComponent<Items>();
+		slots = inventorySlots.GetComponentsInChildren<Slot>(); //Получение всех ячеек
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.I))
+		{
+			UpdateUI(); //Обновление интерфейса
+			canvas.enabled = !canvas.enabled;
+		}
+
+
+	}
+
+	void UpdateUI()
+	{
+		/*//Debug.Log(slots.Length);
+		for (int i = 0; i < 4;i++)
+		{
+			if (item)
+		}*/
+		slots[0].sprite = item;
+		Debug.Log(slots[0].sprite);
+		for (int i = 0; i < slots.Length; i++) //Проверка всех предметов
+		{
+			bool active = false;
+			if (items.hasItems[i]) //Если такой предмет есть у пользователя, то он будет отображаться в слоте
+			{
+				active = true;
+			}
+
+			slots[i].UpdateSlot(active);
+		}
+	}
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+	public int index = 0; //Номер предмета, в этой игре будет всего 3 типа брони, поэтому писать более сложный скрипт нет смысла
+	public Slot slot;
+	public Inventory inv;
+	public Sprite sprite;
+	void OnTriggerEnter2D(Collider2D obj) //«Наезд» на объект
+	{
+		
+		if (obj.transform.tag == "Player")
+		{
+			//Debug.Log(obj.transform.tag);
+			///obj.GetComponent<Items>().AddItem(index);//Если наехал игрок, то он сможет подобрать предмет
+			inv.item = sprite;
+			Debug.Log(inv.item);
+			Destroy(gameObject); //Удаление объекта с карты
+			//Debug.Log(obj.transform.tag);
+		}
+	}
+}
