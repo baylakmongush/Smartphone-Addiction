ae2a2700ab190b37e7f406606f61c890c84d7ff1
Kim Ilya <kimilya50@gmail.com>
2020-05-03 11:09:44 +0300

    added menus

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManager
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+    	//lad game scene
+    	
+    }
+
+    public void QuitGame(){
+    	Application.Quit();
+    }
+}
