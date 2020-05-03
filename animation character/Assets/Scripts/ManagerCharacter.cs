212f90e2296fc093838522ee29b0f5bf67658abb
Student <mongush.baylak17@gmail.com>
2020-05-03 15:45:24 +0300

    add demo game

--- animation character/Assets/Scripts/ManagerCharacter.cs
+++ animation character/Assets/Scripts/ManagerCharacter.cs
@@ -24,6 +24,7 @@ public class ManagerCharacter : MonoBehaviour
         input = Input.GetAxis("Horizontal");
         input_vert = Input.GetAxis("Vertical");
         Vector2 scale = transform.localScale;
+        position = transform.position;
         Vector2 movement_vector = transform.position;
         if (input > 0 && transform.position.x < 3.91f)
         {
@@ -31,44 +32,34 @@ public class ManagerCharacter : MonoBehaviour
             anim.SetBool("walk", true);
             movement_vector.x += Time.deltaTime * input * 2;
             transform.position = movement_vector;
-            if (transform.position.x < -0.1f && transform.position.x > -2)
+            if (transform.position.x < -0.23f && transform.position.x > -2)
             {
-                position.x += 1;
-                position.y = gameObject.transform.position.y;
+                position.x = -0.1f;
+                transform.position = position;
+            }
+            if (transform.position.x < -8.82f && transform.position.x > -10.66)
+            {
+                position.x = -8.82f;
                 transform.position = position;
             }
         }
-        else if (input < 0 && transform.position.x > -8.35f)
+        else if (input < 0 && transform.position.x > -14.02f)
         {
             anim.SetBool("direction", false);
             anim.SetBool("walk", true);
             movement_vector.x += Time.deltaTime * input * 2;
             transform.position = movement_vector;
-            if (transform.position.x < -0.1f && transform.position.x > -2)
+            if (transform.position.x < -0.23f && transform.position.x > -2)
             {
-                position.x -= 1;
-                position.y = gameObject.transform.position.y;
+                position.x = -2;
+                transform.position = position;
+            }
+            if (transform.position.x < -8.82f && transform.position.x > -10.66)
+            {
+                position.x = -10.66f;
                 transform.position = position;
             }
         }
-        /*else if (input_vert > 0 )
-        {
-            scale.x -= 0.01f;
-            scale.y -= 0.01f;
-            movement_vector.y += Time.deltaTime * input_vert * 0.6f;
-            anim.SetBool("walk", true);
-            transform.localScale = scale;
-            transform.position = movement_vector;
-        }
-        else if (input_vert < 0)
-        {
-            scale.x += 0.01f;
-            scale.y += 0.01f;
-            movement_vector.y += Time.deltaTime * input_vert * 0.6f;
-            anim.SetBool("walk", true);
-            transform.localScale = scale;
-            transform.position = movement_vector;
-        }*/
         else
             anim.SetBool("walk", false);
         if (input == 0 && input_vert == 0)
